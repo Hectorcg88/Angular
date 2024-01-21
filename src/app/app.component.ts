377c0917d151e0ec4b033c514a84e4d85a3fb4d6
Hectorcg88 <casesgimenezh@gmail.com>
2024-01-21 17:40:18 +0100

    Hecho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { ImcCalculatorComponent } from './imc-calculator/imc-calculator.component';
+import { FormData } from './interfaces/form-data';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormComponent, ImcCalculatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'practica';
+  formData?: FormData;
+
+  updateFormData(data: FormData) {
+    this.formData = data;
+  }
 }
